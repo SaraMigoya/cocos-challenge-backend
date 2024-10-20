424e3d3520c571f5d698d118e2e4e089032040a4
SaraMigoya <sara.migoya@gojiraf.ai>
2024-10-20 11:54:12 -0300

    feat: add search by term and paging for Intruments

--- src/controllers/InstrumentController.ts
+++ src/controllers/InstrumentController.ts
@@ -1,12 +1,13 @@
 import { NextFunction, Request, Response } from 'express';
 import { InstrumentService } from '../services/InstrumentService';
 
-//TODO: Implementar busqueda parcial por termino
 export class InstrumentController {
     static async searchInstruments(req: Request, res: Response, next: NextFunction): Promise<void> {
         const { query } = req.query;
+        const page = parseInt(req.query.page as string) || 1;
+        const limit = parseInt(req.query.limit as string) || 10;
         try {
-            const instruments = await InstrumentService.searchInstruments(query as string);
+            const instruments = await InstrumentService.searchInstruments(query as string, page, limit);
             res.status(200).json(instruments);
         } catch (error) {
             next(error);
--- src/services/InstrumentService.ts
+++ src/services/InstrumentService.ts
@@ -2,12 +2,20 @@ import { AppDataSource } from '../database/db';
 import { Instrument } from '../entities/instrument.entity';
 
 export class InstrumentService {
-    static async searchInstruments(query: string) {
+    static async searchInstruments(query: string, page: number = 1, limit: number = 10) {
         const instrumentRepository = AppDataSource.getRepository(Instrument);
+        const removeBlankSpaces = query.trim();
+        if (!removeBlankSpaces) {
+            return [];
+        }
+
+        const offset = (page - 1) * limit;
 
         return instrumentRepository
             .createQueryBuilder('instrument')
-            .where('instrument.ticker ILIKE :query OR instrument.name ILIKE :query', { query: `%${query}%` })
+            .where('instrument.ticker ILIKE :query OR instrument.name ILIKE :query', { query: `%${removeBlankSpaces}%` })
+            .offset(offset)
+            .limit(limit)
             .getMany();
     }
 }
